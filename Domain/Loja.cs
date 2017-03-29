e99535baee7a9873606d684d7497f637fc4caab4
Rafaela <rafaela.vm@gmail.com>
2017-03-29 19:15:28 -0300

    Trabalhando com Data Annotations

--- Domain/Loja.cs
+++ Domain/Loja.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -11,8 +12,15 @@ namespace Domain
     [Table("Loja")]
     public class Loja
     {
+        //o key nao seria necessario pq o entity ja entende q id é o id da tabela
+        [Key]
         public int Id { get; set; }
+
+        [MaxLength(200)]
+        [Required]
         public string Nome { get; set; }
+
+        [MaxLength(2000)]
         public string Descricao { get; set; }
     }
 }
--- Domain/Produto.cs
+++ Domain/Produto.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +12,19 @@ namespace Domain
     [Table("Produto")]
     public class Produto
     {
+        //o key nao seria necessario pq o entity ja entende q id é o id da tabela
+        [Key]
         public int Id { get; set; }
+        
+        [MaxLength(200)]
+        [Required]
         public string Nome { get; set; }
+
+        [MaxLength(2000)]
+        [Required]
         public string Descricao { get; set; }
+
+        [Range(-999999999999.99, 999999999999.99)]
         public decimal Valor { get; set; }
     }
 }
